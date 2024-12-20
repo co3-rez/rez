ff8e7a9996050b84a3bdf63234c67de79ab99d71
alex.haduch <alex.haduch@company3.com>
2024-12-19 21:20:57 -0500

    WIP test ordered packages with ephemeral variants

---
+++ src/rez/data/tests/solver/packages/pyvariants_eph/2/package.py
@@ -0,0 +1,12 @@
+name = "pyvariants_eph"
+version = "2"
+
+variants = [
+    ["python-2.7.0"],
+    ["python-2.6.8", "nada"],
+    ["python-2.6.8"],
+    ["nada"],
+    ["python-2.7.0", ".cxx11abi-1"],
+]
+
+timestamp = 1000
